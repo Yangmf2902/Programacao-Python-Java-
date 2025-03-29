4b1894442020e7b837b349f723d6a93828a0a762
Yangmf2902 <ygmf022908@gmail.com>
2025-03-29 20:00:09 -0300

    Update ex10.py

--- Atividade01/ex10.py
+++ Atividade01/ex10.py
@@ -1,10 +1,7 @@
 n = int(input("Digite um número inteiro maior que 1: "))
 while n <= 1:
 	n = int(input("Digite um número inteiro maior que 1: "))
-divisores = []
-for x in range(1, n+1):
-	if n % x == 0:
-		divisores.append(x)
+divisores = [x for x in range(1, n+1) if n % x == 0] 
 if len(divisores) > 2:
 	print("O número digitado não é primo.")
 elif len(divisores) == 2:
