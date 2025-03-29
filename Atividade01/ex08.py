e42e7d8a36dc5f0a5443932bc3fc2902f583cb21
Yangmf2902 <ygmf022908@gmail.com>
2025-03-29 19:19:16 -0300

    Create ex08.py

---
+++ Atividade01/ex08.py
@@ -0,0 +1,3 @@
+celsius = float(input("Digite uma temperatura em celsius que será convertida en Fahrenheit: "))
+f = (celsius*(5/9)) + 32
+print(f"A temperatura em Fahrenheit é igual a: {f} graus.")
