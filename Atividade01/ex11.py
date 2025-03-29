380ab917264adc22b30de81893d9a4e4fe8f4072
Yangmf2902 <ygmf022908@gmail.com>
2025-03-29 20:15:54 -0300

    Create ex11.py

---
+++ Atividade01/ex11.py
@@ -0,0 +1,9 @@
+id = input("Digite seu nome de usuário: ")
+senha = input("Digite sua senha: ")
+
+while id == senha:
+	print("Seu nome de usuário precisa ser diferentebda sua senha!!! Tente novamente. ")
+	id = input("Digite seu nome de usuário: ")
+	senha = input("Digite sua senha: ")
+    
+print("Conta criada, seja bem-vindo.")
